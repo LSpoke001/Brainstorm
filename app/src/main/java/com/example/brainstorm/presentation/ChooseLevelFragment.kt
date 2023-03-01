66ede6b51aa8d7ec4df50f4ea2941288016e90f3
Elena Spytsyna <lspoke.larn@gmail.com>
2023-03-01 17:19:52 +0200

    added graph

--- app/src/main/java/com/example/brainstorm/presentation/ChooseLevelFragment.kt
+++ app/src/main/java/com/example/brainstorm/presentation/ChooseLevelFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.brainstorm.R
 import com.example.brainstorm.databinding.FragmentChooseLevelBinding
 import com.example.brainstorm.databinding.FragmentWelcomeBinding
@@ -44,10 +45,10 @@ class ChooseLevelFragment: Fragment() {
     }
 
     private fun launchGameFragment(level: Level) {
-        requireActivity().supportFragmentManager.beginTransaction()
-            .replace(R.id.main_container, GameFragment.newInstance(level))
-            .addToBackStack(GameFragment.NAME)
-            .commit()
+        val arg = Bundle().apply {
+            putParcelable(GameFragment.KEY_LEVEL, level)
+        }
+        findNavController().navigate(R.id.action_chooseLevelFragment_to_gameFragment2, arg)
     }
 
 
--- app/src/main/java/com/example/brainstorm/presentation/GameFragment.kt
+++ app/src/main/java/com/example/brainstorm/presentation/GameFragment.kt
@@ -10,6 +10,7 @@ import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory
+import androidx.navigation.fragment.findNavController
 import com.example.brainstorm.R
 import com.example.brainstorm.databinding.FragmentGameBinding
 import com.example.brainstorm.domain.entity.GameResult
@@ -120,16 +121,16 @@ class GameFragment : Fragment() {
     }
 
     private fun launchGameFinishedFragment(gameResult: GameResult) {
-        requireActivity().supportFragmentManager.beginTransaction()
-            .replace(R.id.main_container, GameFinishedFragment.newInstance(gameResult))
-            .addToBackStack(null)
-            .commit()
+        val arg = Bundle().apply {
+            putParcelable(GameFinishedFragment.KEY_GAME_RESULT, gameResult)
+        }
+        findNavController().navigate(R.id.action_gameFragment2_to_gameFinishedFragment, arg)
     }
 
     companion object {
 
         const val NAME = "GameFragment"
-        private const val KEY_LEVEL = "level"
+        const val KEY_LEVEL = "level"
 
         fun newInstance(level: Level): GameFragment {
             return GameFragment().apply {
