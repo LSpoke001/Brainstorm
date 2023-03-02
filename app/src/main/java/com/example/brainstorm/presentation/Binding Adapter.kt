c0dd525556054e12e4f48d18868cdf22dede5908
Elena Spytsyna <lspoke.larn@gmail.com>
2023-03-02 18:15:30 +0200

    binding game fragment

--- app/src/main/java/com/example/brainstorm/presentation/Binding Adapter.kt
+++ app/src/main/java/com/example/brainstorm/presentation/Binding Adapter.kt
@@ -1,11 +1,18 @@
 package com.example.brainstorm.presentation
 
+import android.content.Context
+import android.content.res.ColorStateList
 import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
 import com.example.brainstorm.R
 import com.example.brainstorm.domain.entity.GameResult
 
+interface OnOptionClickListener{
+    fun onOptionClick(option: Int)
+}
 @BindingAdapter("requiredAnswers")
 fun bindRequireAnswers(textView: TextView, count: Int) {
     textView.text = String.format(
@@ -53,3 +60,34 @@ fun bindEmojiResult(imageView: ImageView, gameResult: GameResult) {
     }
     imageView.setImageResource(imgId)
 }
+@BindingAdapter("enoughPercent")
+fun bindEnoughPercent(progressBar: ProgressBar, enough: Boolean) {
+    val color = getColorByState(progressBar.context, enough)
+    progressBar.progressTintList = ColorStateList.valueOf(color)
+}
+@BindingAdapter("enoughCount")
+fun bindEnoughCount(textView: TextView, enough: Boolean) {
+    textView.setTextColor(getColorByState(textView.context, enough))
+}
+
+@BindingAdapter("textAsString")
+fun bindEnoughCount(textView: TextView, number: Int) {
+    textView.text = number.toString()
+}
+
+@BindingAdapter("onOptionClickListener")
+fun bindOnOptionClickListener(textView: TextView, clickListener: OnOptionClickListener) {
+    textView.setOnClickListener {
+        clickListener.onOptionClick(textView.text.toString().toInt())
+    }
+}
+private fun getColorByState(context: Context, goodState: Boolean): Int {
+    val colorResId = if (goodState) {
+        android.R.color.holo_green_light
+    } else {
+        android.R.color.holo_red_light
+    }
+    return ContextCompat.getColor(context, colorResId)
+}
+
+
